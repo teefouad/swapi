456650760aeb6d06004bc5b3f8c08af22810ec2b
MostafaFouad <tee.fouad@gmail.com>
2018-01-24 23:09:38 +0200

    Setup react

--- src/index.js
+++ src/index.js
@@ -1,18 +1,12 @@
 /**
  * index.js
  */
-
-import styles from './styles.css';
-import { add, subtract } from './module';
+import React from 'react';
+import { render } from 'react-dom';
 
 if (module.hot) {
   module.hot.accept();
 }
 
-const resultA = add(2, 3);
-const resultB = subtract(5, 1);
-
-console.log(resultA, resultB);
-console.log(styles.localClass); // _19OBmKu4X8SmIISJiYXz8U
-console.log(styles.globalClass); // undefined
-console.log(process.env.APP_NAME); // 'dolphin'
+// render the app
+render(<div>Hello React!</div>, document.getElementById('app'));
--- src/module.js
+++
@@ -1,7 +0,0 @@
-/**
- * module.js
- */
-
-export const add = (a, b) => a + b;
-export const subtract = (a, b) => a - b;
-export const createObject = o => ({ ...o, anotherOption: 'Hi!' });
